e5ca7c02ceb7278a00b44f9a6903e58deca837ae
julian soro <juliansoro.dev@gmail.com>
2024-02-23 17:42:46 +0100

    dev: read OPENAI_API_KEY from env vars

---
+++ src/constants.py
@@ -0,0 +1,3 @@
+from os import environ
+
+API_KEY = environ["OPENAI_API_KEY"]
---
+++ src/main.py
@@ -0,0 +1,5 @@
+from constants import API_KEY
+
+C_GREEN = "\033[92m"
+C_END = "\033[0m"
+print(f"Hello world, your {C_GREEN}OPENAI API KEY{C_END} is: \"{API_KEY}\"")
